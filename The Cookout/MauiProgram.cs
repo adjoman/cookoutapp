8a432e6c6e4800aa1b49a5cf64c277a4c393fe74
Calvin Brown <calvin@kairuconsulting.com>
2023-07-21 17:49:36 -0400

    login screen added

--- The Cookout/MauiProgram.cs
+++ The Cookout/MauiProgram.cs
@@ -13,7 +13,9 @@ public static class MauiProgram
             .UseMauiCommunityToolkit()
             .ConfigureFonts(fonts =>
 			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("Antone-Block.otf", "AntoneBlock");
+                fonts.AddFont("Antone-Clean.otf", "AntoneClean");
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
 
